62d119c4434bddd27a07b9c244a1723b417be22a
wanshengdao <wanshengdao@xgd.com>
2024-06-14 14:35:55 +0800

    feat: 文件模式设置可以指定文件夹

--- src/main/java/cn/karent/common/TemplateConfig.java
+++ src/main/java/cn/karent/common/TemplateConfig.java
@@ -1,12 +1,18 @@
 package cn.karent.common;
 
+import freemarker.cache.FileTemplateLoader;
 import freemarker.cache.StringTemplateLoader;
+import io.micrometer.common.util.StringUtils;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.Assert;
 import org.springframework.validation.annotation.Validated;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Locale;
 
 /**
@@ -30,6 +36,11 @@ public class TemplateConfig {
         private TemplateLoadMode mode;
 
         /**
+         * 如果是文件模式, 这里是保存的模板路径
+         */
+        private String templatePath;
+
+        /**
          * 是否是字符串模式
          *
          * @return true- string模式
@@ -42,10 +53,11 @@ public class TemplateConfig {
 
 
     @Bean
-    public freemarker.template.Configuration configuration(Config config) {
+    public freemarker.template.Configuration configuration(Config config) throws IOException {
         freemarker.template.Configuration configuration = new freemarker.template.Configuration(freemarker.template.Configuration.VERSION_2_3_33);
         if (TemplateLoadMode.FILE.equals(config.getMode())) {
-            configuration.setClassForTemplateLoading(TemplateConfig.class, "/api");
+            Assert.isTrue(StringUtils.isNotBlank(config.getTemplatePath()), "文件模式下模板路径不能为空");
+            configuration.setTemplateLoader(new FileTemplateLoader(new File(config.getTemplatePath())));
         } else {
             configuration.setTemplateLoader(new StringTemplateLoader());
         }
