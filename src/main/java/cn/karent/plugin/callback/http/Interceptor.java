3fb2250a7f6e3fc45fa0e26ec7d07228c98df1f9
karentwan <xhxiaodao@163.com>
2024-06-30 11:46:30 +0800

    修复插件为空的情况

--- src/main/java/cn/karent/plugin/callback/http/Interceptor.java
+++ src/main/java/cn/karent/plugin/callback/http/Interceptor.java
@@ -3,6 +3,7 @@ package cn.karent.plugin.callback.http;
 import cn.karent.plugin.callback.ConfiguredRequest;
 import cn.karent.util.JsonUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
@@ -17,6 +18,7 @@ import java.util.Objects;
  * @date 2024/6/30
  */
 @RequiredArgsConstructor
+@Slf4j
 public class Interceptor implements ClientHttpRequestInterceptor {
 
     public static final String INTERCEPTOR_NAME = "name";
@@ -25,10 +27,20 @@ public class Interceptor implements ClientHttpRequestInterceptor {
 
     @Override
     public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+        log.debug("开始restTemplate拦截器处理");
         String s = new String(body, StandardCharsets.UTF_8);
         ConfiguredRequest configuredRequest = JsonUtils.parseObject(s, ConfiguredRequest.class);
         Map<String, Object> config = configuredRequest.getConfig();
-        byte[] bytes = JsonUtils.toBytes(configuredRequest.getBody());
+        Object content = configuredRequest.getBody();
+        byte[] bytes = null;
+        if (content instanceof String c) {
+            bytes = c.getBytes(StandardCharsets.UTF_8);
+        } else {
+            bytes = JsonUtils.toBytes(content);
+        }
+        if (Objects.isNull(config)) {
+            return execution.execute(request, bytes);
+        }
         String name = (String) config.get(INTERCEPTOR_NAME);
         CallbackInterceptor callbackInterceptor = interceptorMap.get(name);
         if (!Objects.isNull(callbackInterceptor)) {
